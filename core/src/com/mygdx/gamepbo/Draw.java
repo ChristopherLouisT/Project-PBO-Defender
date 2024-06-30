2edb841fecd5895ef6d4acf5b5f3a27cbe2d43ab
ChristopherLouisT <142294432+ChristopherLouisT@users.noreply.github.com>
2024-06-30 22:23:17 +0700

    Delete core directory

--- core/src/com/mygdx/gamepbo/Draw.java
+++
@@ -1,7 +0,0 @@
-package com.mygdx.gamepbo;
-
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
-public interface Draw {
-    abstract public void Draw (SpriteBatch batch);
-}
--- core/src/com/mygdx/gamepbo/Update.java
+++
@@ -1,5 +0,0 @@
-package com.mygdx.gamepbo;
-
-public interface Update {
-    abstract void Update(float deltaTime);
-}
